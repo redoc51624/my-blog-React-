b784f781c6a00405ff819d2b7b017c3b15d0e3b6
vikas kumar <vikas10145.12@bitmesra.ac.in>
2017-07-26 17:20:47 +0530

    inital commit to firebase login

--- src/About.js
+++ src/About.js
@@ -3,21 +3,31 @@ import React from 'react';
 import './index.css';
 
 class About extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      header1 = something for h1;
+       header2 = something for h2;
+        header3 = something for h3;
+        para1 = some data in paragraph 1;
+        para2 = some data in paragraph 2;
+        para3 = some data in paragraph 3;
+    }
+  }
   render() {
     return (
       <div>
        <div className="contents">
            <ul>
-           <li> <h1>Header 1</h1>
-               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+           <li> <h1>{this.state.header1}</h1>
+               <p>{this.state.para1}</p>
            </li>
-           <li> <h1>Header 1</h1>
-               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+             <li> <h1>{this.state.header2}</h1>
+               <p>{this.state.para2}</p>
            </li>
-           <li> <h1>Header 1</h1>
-               <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <li> <h1>{this.state.header3}</h1>
+               <p>{this.state.para3}</p>
            </li>
-
            </ul>
        </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as firebase from 'firebase';
+import *as firebase from 'firebase';
 import registerServiceWorker from './registerServiceWorker';
 
 // Initialize Firebase
+(function(){
 const firebaseConfig = {
   apiKey: "AIzaSyBHNVdUsePqzL9tTB_F_sMwW71pGqUMZXY",
   authDomain: "react-blog-4e990.firebaseapp.com",
@@ -14,7 +15,49 @@ const firebaseConfig = {
   storageBucket: "react-blog-4e990.appspot.com",
   messagingSenderId: "261244525843"
 };
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const txtEmail = document.getElementById('txtEmail');
+const password = document.getElementById('pwd');
+const btnlogin = document.getElementById('btnSignin');
+const btnsignup = document.getElementById('btnSignUp');
+const btnlogout = document.getElementById('btnLogout');  
+
+//add login event
+btnlogin.addEventListener('click', e =>{
+	const email = txtEmail.value;
+	const pass = password.value;
+	const auth = firebase.auth();
+	const promise = auth.signInWithEmailAndPassword('email','pass');
+	promise.catch(e=>console.log(e.message));
+});
+
+//Sign up evnet
+btnlogin.addEventListener('click', e =>{
+	const email = txtEmail.value;
+	const pass = password.value;
+	const auth = firebase.auth();
+	const promise = auth.createUserWithEmailAndPassword('email','pass');
+	promise.catch(e=>console.log(e.message));
+});
+
+//Signout event
+btnlogout.addEventListener('click', e=>{
+	firebase.auth().signOut();
+});
+
+//add realtime listner
+firebase.auth().onAuthStateChanged(firebaseUser=>{
+	if(firebaseUser){
+		console.log('firebase user');
+	}
+	else
+	{
+		console.log('Not logged in');
+	}
+});
+}());
+
 
 ReactDOM.render(<App headerProp = "Header from props..." contentProp = "Content from props..."/>,
 document.getElementById('root')
